77c97b7603f7240c4abe245cf06a09e2ac1f3756
Hussam Burqan <hussamhasmhasm@gmail.com>
2025-05-08 18:44:12 +0300

    chore(data-structure): remove obsolete file

--- bin/dsa_dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dsa_dart/dsa_dart.dart' as dsa_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dsa_dart.calculate()}!');
-}
--- test/dsa_dart_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dsa_dart/dsa_dart.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
